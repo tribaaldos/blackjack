c71bb04b49ffab5b6fd3e099f6406c7d97037b5a
Angel <angel@angel.domain.com>
2023-05-09 16:48:27 -0700

    tuesday commit 5pm

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /*----- constants -----*/
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const dealerMessages = ['Player wins', 'Dealer wins', 'Player above 21, lose', 'Dealer above 21, lose', 'Tie', 'Blackjack']
+const playerMessages = ['Player wins', 'Player above 21, lose', 'Player loses', 'Tie', 'Blackjack']
 // Build an 'original' deck of 'card' objects used to create shuffled decks
 const originalDeck = buildOriginalDeck();
 
@@ -9,17 +11,22 @@ let playerCards;
 let dealerCards;
 let cards;
 let hands;
+let playerCardsSum = 0;
+let dealerCardsSum = 0;
 
 /*----- cached element references -----*/
 const dealer_cards_div = document.querySelector('.dealer-cards');
-const player_cards_div = document.querySelector('.player-cards')
-const bet_button = document.querySelector('#bet')
-const hide_button = document.getElementById('bet')
+const player_cards_div = document.querySelector('.player-cards');
+const number_player = document.querySelector('.number-player')
+const number_dealer = document.querySelector('.number-dealer')
+const bet_button = document.querySelector('#bet');
+const hide_button = document.getElementById('bet');
 
 /*----- event listeners -----*/
 // document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
 document.querySelector('#bet').addEventListener('click', dealCards);
 document.querySelector('#hit').addEventListener('click', botonHit);
+document.querySelector('#stay').addEventListener('click', handleStand);
 document.querySelector('#bet').addEventListener('click', hideButton);
 
 //------remove Event Listeners
@@ -36,8 +43,20 @@ function init(){
 }
 init();
 function render() {
+  //show cards
   dealer_cards_div.innerHTML = dealerCards.map(card => `<img class="card ${card.face}"></div>`).join('');
   player_cards_div.innerHTML = playerCards.map(card => `<img class="card ${card.face}"></div>`).join('');
+  // show number of the value of all the cards
+  //number of player cards
+  playerCards.forEach(card => playerCardsSum += card.value);
+  number_player.innerHTML = playerCardsSum;
+  //number of dealer cards
+  dealerCards.forEach(card => dealerCardsSum += card.value);
+  number_dealer.innerHTML = dealerCardsSum;
+
+
+  
+
 }
 
 function dealCards() {
@@ -55,29 +74,40 @@ function hideButton() {
 
 function botonHit() {
   
-  let dealerCardsSum = 0;
   let playerCardsSum = 0;
-
-  dealerCards.forEach(card => dealerCardsSum += card.value); 
+  
   playerCards.forEach(card => playerCardsSum += card.value);
-
- if (playerCardsSum < 21) {
-  playerCards.push(hands.pop());
-
-} else if (playerCardsSum > 21) {
-  playerCardsSum += playerCards[playerCards.length - 1].value;
-  console.log(playerCardsSum, 'NOOOO :(! player above 21');
-} 
-  while (dealerCardsSum < 16) { 
-    dealerCards.push(hands.pop());
-    dealerCardsSum += dealerCards[dealerCards.length - 1].value;
-} 
-  if (dealerCardsSum > 21) {
-    console.log('Dealer loses!');
-  }
-
+  
+  if (playerCardsSum < 21) {
+    playerCards.push(hands.pop());
+    
+  } else if (playerCardsSum > 21) {
+    playerCardsSum += playerCards[playerCards.length - 1].value;
+    console.log(playerCardsSum, playerMessages[1]);
+  } 
+  
   render();
 }
+function handleStand() {
+
+  let dealerCardsSum = 0;
+  dealerCards.forEach(card => dealerCardsSum += card.value); 
+
+  if (dealerCardsSum === 21) {
+    console.log('Dealer has 21!');
+  } else {
+
+    while (dealerCardsSum < 17) { 
+      dealerCards.push(hands.pop());
+      dealerCardsSum += dealerCards[dealerCards.length - 1].value;
+    } 
+    if (dealerCardsSum > 21) {
+      console.log('Dealer busts!');
+    }
+  }
+  render();
+}
+
 
 
 
@@ -109,7 +139,7 @@ function buildOriginalDeck() {
         // The 'face' property maps to the library's CSS classes for cards
         face: `${suit}${rank}`,
         // Setting the 'value' property for game of blackjack, not war
-        value: Number(rank) || (rank === 'A' ? 11 : 10)
+        value: Number(rank) || (rank === 'A' ? 11 && 1 : 10)
       });
     });
   });
